f28827175e4b457f9e110a7ca8388aba5f427072
Sadadi Assani <sadidassani84@gmail.com>
2024-04-08 12:32:30 +0200

    init project

---
+++ src/model/Adresse.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Adresse {
+	public String libelleRue;
+	public int numeroRue;
+	public int codePostal;
+	public String ville;
+
+}
---
+++ src/model/Personne.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Personne {
+	public String nom;
+	public String prenom;
+	public int age;
+	public String sexe;
+	public Adresse adresse;
+
+}
